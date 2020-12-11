2cb6580392d0b6abcc6e87e9e32cc086ac1613db
SanduniMaheshika <s15924@sci.pdn.ac.lk>
2020-12-11 15:15:06 +0530

    create all componets in angular

--- frontend-gym-management-system/src/app/app.module.ts
+++ frontend-gym-management-system/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TrainerComponent } from './trainer/trainer.component';
+import { TrainorComponent } from './trainor/trainor.component';
+import { TraineeComponent } from './trainee/trainee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    LoginComponent,
+    SignUpComponent,
+    TrainerComponent,
+    TrainorComponent,
+    TraineeComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend-gym-management-system/src/app/trainee/trainee.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TraineeComponent } from './trainee.component';
+
+describe('TraineeComponent', () => {
+  let component: TraineeComponent;
+  let fixture: ComponentFixture<TraineeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TraineeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TraineeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
