6a64cf78e4797571911cc87fca3665aedee8a057
ThakshilaDulanjali <s15915@sci.pdn.ac.lk>
2020-12-15 17:07:39 +0530

    Backend og GMS

---
+++ backend-gym-management-system/src/test/java/com/io/fitandlift/FitAndLiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.fitandlift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitAndLiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
