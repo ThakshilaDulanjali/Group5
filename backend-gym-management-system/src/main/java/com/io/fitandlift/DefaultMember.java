fd0294c3d280b42f14a0a4b4ebce34eb2160f0e2
ShalikaDasanayaka <s15906@sci.pdn.ac.lk>
2020-12-18 17:41:02 +0530

    DefaultMember

--- backend-gym-management-system/src/main/java/com/io/fitandlift/DefaultMember.java
+++ backend-gym-management-system/src/main/java/com/io/fitandlift/DefaultMember.java
@@ -1,5 +1,41 @@
 package com.io.fitandlift;
 
-public class DefaultMember {
-
+public abstract class DefaultMember {
+		protected String membershipNumber;
+		protected String name;
+		protected Date startMembershipDate;
+		protected DigDecimal annualFee;
+		
+		
+		public DefaultMember(String membershipNumber, String name,Date startMembershipDate, DigDecimal annualFee) {
+			this.membershipNumber = membershipNumber;
+			this.name = name;
+			this.startMembershipDate = startMembershipDate;
+			this.annualFee = annualFee;
+			
+		}
+		
+		public String getMembershipNumber() {return membershipNumber;}
+		public void setMembershipNumber(String membershipNumber) {this.membershipNumber = membershipNumber;}
+		public string getName() {return name;}
+		public void setName(String name) {this.name = name;}
+		public Data getstartMembershipDate() {return startMembershipDate;)
+		public void setStartMembershipDate(Date startMembershipDate) {this.startMembershipDate = startmembershipDate;}
+		public DigDecimal getAnualFee() {return annualFee;}
+		publiv void setAnnualFee(DigDecimal annualfee) {this.annualFee = annualFee;}
+		
+		
+		
+		@override
+		public String toString() {
+			return "DefaultMember{"+
+					"membershipNumber = " + membershipNumber + " \ " + 
+					", name " + name + " \ " +
+					",startMembershipDate = " + startMembershipDate + " \ " +
+					",annualFee" + annualFee +
+					"{" + super.toString();
+		
+		}
+		
+		
 }
