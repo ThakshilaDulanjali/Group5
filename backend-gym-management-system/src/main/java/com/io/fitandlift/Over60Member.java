fc34994772b44a409948e3819c46894756d3116f
Prasadi Fernando <s15912@sci.pdn.ac.lk>
2020-12-18 17:47:21 +0530

    Over60Member

--- backend-gym-management-system/src/main/java/com/io/fitandlift/Over60Member.java
+++ backend-gym-management-system/src/main/java/com/io/fitandlift/Over60Member.java
@@ -1,5 +1,38 @@
 package com.io.fitandlift;
 
-public class Over60Member {
+import java.math.BigDecimal;
+import java.sql.Date;
 
+public class Over60Member extends DefaultMember {
+	protected int age;
+	
+	public Over60Member(String membershipNumber, String name, Date startMembershipDate, BigDecimal annualFee, int age)
+	{
+		super();
+		this.age = age;
+	}
+	
+	public int getAge() {return age;}
+	
+	public void setAge(int age) {this.age = age;}
+	
+	
+	@Override
+	public String toString() {
+		
+		String membershipNumber = null;
+		String name = null;
+		String startMembershipDate = null;
+		String annualFee = null;
+		return "Over60Member{" +
+				"age=" + age +
+				",membershipNumber='" + membershipNumber + '\''+
+				",name=" + name + '\'' +
+				",startMembershipDate='" + startMembershipDate + '\'' +
+				",annualFee" + annualFee +
+				"}" + super.toString();
+				
+		
+		
+	}	
 }
