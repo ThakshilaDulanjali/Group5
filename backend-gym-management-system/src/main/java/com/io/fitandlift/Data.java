7e267ca5d5b16eae21288165e7c641dedddb1e01
PrabathPrasanga <s15933@sci.pdn.ac.lk>
2020-12-18 12:30:28 +0530

    Dateclass

--- backend-gym-management-system/src/main/java/com/io/fitandlift/Data.java
+++ backend-gym-management-system/src/main/java/com/io/fitandlift/Data.java
@@ -1,5 +1,38 @@
 package com.io.fitandlift;
 
 public class Data {
+	private int year;
+	private int month;
+	private int day;
+	
+	
+	public Data(int year, int month, int day) {
+		
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	
+	public String toString() {return year+ "/" + month + "/"+ day;}
+	
+	
 
 }
