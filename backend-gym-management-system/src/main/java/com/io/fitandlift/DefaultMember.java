dde07f080050723e045845e76476343d4b33593c
gayani2110 <s15940sci.pdn.ac.lk>
2020-12-19 20:43:59 +0530

    complete student member class

---
+++ backend-gym-management-system/src/main/java/com/io/fitandlift/DefaultMember.java
@@ -0,0 +1,57 @@
+package com.io.fitandlift;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+public abstract class DefaultMember {
+		protected String membershipNumber;
+		protected String name;
+		protected Date startMembershipDate;
+		protected BigDecimal annualFee;
+
+
+
+		public DefaultMember(String membershipNumber, String 
+				name, Date startMembershipDate, BigDecimal annualFee) {
+			this.membershipNumber = membershipNumber;
+			this.name = name;
+			this.startMembershipDate = startMembershipDate;
+			this.annualFee = annualFee;
+
+		}
+
+		public String getMembershipNumber() {return membershipNumber;}
+		public void setMembershipNumber(String membershipNumber) {this.membershipNumber = membershipNumber;}
+		public String getName() {return name;}
+		public void setName(String name) {this.name = name;}
+		public Date getStartMembershipDate() {return startMembershipDate;}
+		public void setStartMembershipDate(Date startMembershipDate) {this.startMembershipDate = startMembershipDate;}
+		public BigDecimal getAnualFee() {return annualFee;}
+		public void setAnnualFee(BigDecimal annualfee, BigDecimal annualFee) {this.annualFee = annualFee;}
+
+
+
+		@Override
+		public String toString() {
+			return "DefaultMember{"+
+					"membershipNumber='" + membershipNumber + '\'' + 
+					", name='" + name + '\'' +
+					",startMembershipDate='" + startMembershipDate + '\'' +
+					",annualFee=" + annualFee +
+					')';
+
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o)return true;
+			if (!(o instanceof DefaultMember))return false;
+			DefaultMember that = (DefaultMember)o;
+			return Objects.equals(membershipNumber, that.membershipNumber)&&
+					Objects.equals(name, that.name)&&
+					Objects.equals(startMembershipDate,  that.startMembershipDate)&&
+					Objects.equals(annualFee, that.annualFee);
+		}
+
+
+}
--- backend-gym-management-system/src/main/java/com/io/fitandlift/StudentMember.java
+++ backend-gym-management-system/src/main/java/com/io/fitandlift/StudentMember.java
@@ -1,13 +1,14 @@
 package com.io.fitandlift;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class StudentMember extends DefaultMember {
 	protected String schoolName;
 	
 	
 	public StudentMember(String membershipNumber, String name, Date startMembershipDate, BigDecimal annualFee, String schoolName ) {
-		super(membershipNumber, name, startMembershipDate, annualFree);
+		super(schoolName, schoolName, startMembershipDate, annualFee);
 		this.schoolName = schoolName;
 	}
 
@@ -17,8 +18,12 @@ public class StudentMember extends DefaultMember {
 
 	public void setSchoolName(String schoolName) { this.schoolName = schoolName; }
 	
-	@override
+	@Override
 	public String toString() {
+		String membershipNumber = null;
+		String name = null;
+		String startMembershipDate = null;
+		String annualFee = null;
 		return "StudentMember{" +
 	           "SchoolName='" + schoolName + '\'' +
 	           ", membershipNumber='" + membershipNumber + '\'' +
